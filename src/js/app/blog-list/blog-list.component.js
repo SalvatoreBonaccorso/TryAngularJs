4bf42979f0497ac27c6a8044177f450f2c98fa41
Salvatore Bonaccorso <salvatore.bonaccorso@neodatagroup.com>
2019-12-09 17:41:59 +0100

    add looping in a template with ng-repeat

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -5,14 +5,16 @@ angular.module('blogList')
         templateUrl: '/src/templates/blog-list.html',
         controller: function ($scope) {
 
-       /*      var blogItems = [
-                {title},
-                {},
-                {},
-                {},
-                {}
-            ]; */
-            $scope.title = 'Hi there!';
+            var blogItems = [
+                {title:"Some Title", id:1, description:"This is a book"},
+                {title:"Tea", id:2, description:"This is a book"},
+                {title:"Lite", id:3, description:"This is a book"},
+                {title:"Coca-Cola", id:4, description:"This is a book"}
+            ];
+
+            $scope.items = blogItems;
+
+            $scope.title = 'Blog List';
             $scope.clicks = 0;
 
             $scope.someClickTest = function () {
